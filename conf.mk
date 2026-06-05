2b6345f2cc786bd8c436a44b449036877d556003
write-a-resume <stu15451056@outlook.com>
2026-06-05 19:50:13 +0800

    Add files via upload

---
+++ conf.mk
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+flavor_dts_file-MP15 = stm32mp157d-rpmsg_test-mx.dts
+flavorlist-MP15 += $(flavor_dts_file-MP15)
+flavorlist-no_cryp += $(flavor_dts_file-MP15)
